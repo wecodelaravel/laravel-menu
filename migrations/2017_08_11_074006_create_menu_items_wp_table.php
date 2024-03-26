0cf0c6983a8389109a0b2bfa9ecfeba3bc73b5ac
WeCodeLaravel <wecodelaravel@gmail.com>
2024-03-26 16:48:54 -0600

    Update 2017_08_11_074006_create_menu_items_wp_table.php

--- migrations/2017_08_11_074006_create_menu_items_wp_table.php
+++ migrations/2017_08_11_074006_create_menu_items_wp_table.php
@@ -22,6 +22,17 @@ class CreateMenuItemsWpTable extends Migration
             $table->string('class')->nullable();
             $table->unsignedBigInteger('menu');
             $table->integer('depth')->default(0);
+
+            $table->boolean('local')->default(1)->nullable();
+            $table->boolean('development')->default(1)->nullable();
+            $table->boolean('stage')->default(0)->nullable();
+            $table->boolean('production')->default(0)->nullable();
+            $table->boolean('marketing')->default(0)->nullable();
+            $table->boolean('logged_in_only')->default(0)->nullable();
+            $table->boolean('icon_only_menu')->default(0)->nullable();
+            $table->string('link')->nullable()->change();
+            $table->string('menu_icon_class')->nullable();
+            
             $table->timestamps();
 
             $table->foreign('menu')->references('id')->on(config('menu.table_prefix') . config('menu.table_name_menus'))
